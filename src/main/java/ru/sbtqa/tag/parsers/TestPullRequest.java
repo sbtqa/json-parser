826f100d4a34131eced5281920ac363c4697a968
Konstantin Maltsev <mkypers@gmail.com>
2017-02-28 14:21:47 +0700

    reduce slack notifications count

--- src/main/java/ru/sbtqa/tag/parsers/TestPullRequest.java
+++ src/main/java/ru/sbtqa/tag/parsers/TestPullRequest.java
@@ -5,7 +5,6 @@ public class TestPullRequest {
     public void thrower() {
         try {
             int i = 19;
-            System.out.println(i);
         } catch (Exception e) {
             System.out.println("blep");
         }
